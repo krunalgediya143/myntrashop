68b1b7e83a5387a54bde717a520da876562672f8
krunalgediya143 <155801327+krunalgediya143@users.noreply.github.com>
2024-01-06 12:16:38 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://hipihop.in/graphql."
+	const endpoint = "https://myntrashop.in/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://hipihop.in/` + encodeURI(path as string)
+					`https://myntrashop.in/` + encodeURI(path as string)
 				}`,
 			},
 		};
